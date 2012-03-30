65b620c741995bac798400efba29277579fb643e
Dominic Campbell <dom.campbell@gmail.com>
2012-03-30 15:05:34 +0100

    User signup complete

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -8,41 +8,41 @@ describe "LayoutLinks" do
 
   it "should have a Home page at '/'" do
     get '/'
-    response.should have_selector('title', :content => @base_title + "Home")
+    response.should have_selector('title', :content => "Home")
   end
   
   it "should have a Contact page at '/contact'" do
     get '/contact'
-    response.should have_selector('title', :content => @base_title + "Contact")
+    response.should have_selector('title', :content => "Contact")
   end
   
   it "should have a About page at '/about'" do
     get '/about'
-    response.should have_selector('title', :content => @base_title + "About")
+    response.should have_selector('title', :content => "About")
   end
   
   it "should have a Contact page at '/help'" do
     get '/help'
-    response.should have_selector('title', :content => @base_title + "Help")
+    response.should have_selector('title', :content => "Help")
   end
   
   it "should have a Contact page at '/signup'" do
     get '/signup'
-    response.should have_selector('title', :content => @base_title + "Sign up")
+    response.should have_selector('title', :content => "Sign up")
   end
   
   it 'should have the right links on the layout' do
     visit root_path
     click_link "About"
-    response.should have_selector('title', :content => @base_title + "About")
+    response.should have_selector('title', :content => "About")
     click_link "Contact"
-    response.should have_selector('title', :content => @base_title + "Contact")
+    response.should have_selector('title', :content => "Contact")
     click_link "Help"
-    response.should have_selector('title', :content => @base_title + "Help")
+    response.should have_selector('title', :content => "Help")
     click_link "Home"
-    response.should have_selector('title', :content => @base_title + "Home")
+    response.should have_selector('title', :content => "Home")
     click_link "Sign up now!"
-    response.should have_selector('title', :content => @base_title + "Sign up")
+    response.should have_selector('title', :content => "Sign up")
   end
   
 end
